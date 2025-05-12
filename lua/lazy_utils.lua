802765a08043dd5910e2fc49ef388b76d11558b6
Rellikeht <michaelhemperek@gmail.com>
2025-05-12 21:47:24 +0200

    update lua functions

--- lua/lazy_utils.lua
+++ lua/lazy_utils.lua
@@ -1,23 +1,22 @@
 do
   local lazy_group_id = 0
-  function get_lazy_group_id()
+  function get_group_id()
     lazy_group_id = lazy_group_id + 1
     return lazy_group_id - 1
   end
-  function get_lazy_group_name()
-    return "lazy_load_group" .. get_lazy_group_id()
+  function get_group_name()
+    return "lazy_load_group" .. get_group_id()
   end
-  function get_lazy_group()
-    return vim.api
-             .nvim_create_augroup(get_lazy_group_name(), {})
+  function get_group()
+    return vim.api.nvim_create_augroup(get_group_name(), {})
   end
 end
 
 local M = {
-  lazy_load_on_startup = function(func)
-    local gid = get_lazy_group()
+  load_on_startup = function(func)
+    local gid = get_group()
     vim.api.nvim_create_autocmd(
-      { "CursorHold", "CursorMoved" }, {
+      {"CursorHold", "CursorMoved"}, {
         pattern = "*",
         group = gid,
         callback = function()
@@ -28,10 +27,10 @@ local M = {
     )
   end,
 
-  lazy_load_on_filetypes = function(filetypes, func)
-    local gid = get_lazy_group()
+  load_on_filetypes = function(filetypes, func)
+    local gid = get_group()
     vim.api.nvim_create_autocmd(
-      { "FileType" }, {
+      {"FileType"}, {
         pattern = filetypes,
         group = gid,
         callback = function()
@@ -43,16 +42,10 @@ local M = {
   end,
 
   replace_keys = function(keys)
-    --
+    -- easy I guess
+    return vim.api
+             .nvim_replace_termcodes(keys, true, true, true)
   end,
-  -- let l:keys = substitute(a:keys, "<leader>", g:mapleader, "g")
-  -- let l:keys = substitute(l:keys, "<space>", " ", "g")
-  -- let l:keys = substitute(l:keys, "<cr>", "\<CR>", "g")
-  -- for l in "qwertyuiopasdfghjklzxcvbnm_^"
-  --   exe $"let l:keys = substitute(l:keys, '<c-{l}>', '{"\<c-"..l..">"}', 'g')"
-  -- endfor
-  -- return l:keys
-  -- endfunction
 
   --
 }
